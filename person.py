ba1b58ab5450f1b13750edb00eb22c64071ae1ba
Sanghyup Lee <slee548@jhu.edu>
2024-11-17 16:22:48 -0500

    added main and finished person

---
+++ person.py
@@ -0,0 +1,32 @@
+class Person:
+    def __init__(self) -> None:
+        self.is_poi = False     # Boolean to check if someone is in POI
+        self.visited = {}       # HashMap of visited POIs and visited numbers
+        self.total_visited = 0  # Total number of visited POIs
+        self.curr_poi = ""      # Current POI
+        self.hour_stayed = 0    # Hours stayed at current POI
+
+    def visit(self, poi: str):
+        """Logs a visit to a point of interest."""
+        if poi in self.visited:
+            self.visited[poi] += 1
+        else:
+            self.visited[poi] = 0
+        self.total_visited += 1
+        self.is_poi = True
+        self.curr_poi = poi
+        self.hour_stayed += 1
+
+    def leave(self):
+        """Leaves a point of interest."""
+        self.is_poi = False
+        self.hour_stayed = 0
+
+    def __repr__(self) -> str:
+        return (f"Person(is_poi={self.is_poi}, curr_poi='{self.curr_poi}', "
+                f"hour_stayed={self.hour_stayed}, totalVisited={self.totalVisited}, "
+                f"visited={self.visited})")
+
+
+
+    
